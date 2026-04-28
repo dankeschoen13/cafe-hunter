31ad204f6cb742fd8bb49055b7bd3c9cd5bfdb5d
dankeschoen13 <marcobernacer@gmail.com>
2026-04-28 22:33:15 +0800

    fix: add SQLAlchemy naming conventions to resolve Alembic constraint errors

--- app/extensions.py
+++ app/extensions.py
@@ -1,13 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=naming_convention)
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
