149ec68522c91c928e4773575a0518637568d6c6
dankeschoen13 <marcobernacer@gmail.com>
2026-05-01 21:59:28 +0800

    fix: resolve CKEditor version pop-up bug caused my mismatched form name

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ if db_url and db_url.startswith("postgres://"):
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-only-not-safe')
-    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///cafes.db"
+    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///backup_cafes.db"
     DEMO_MODE = os.environ.get('DEMO_MODE') == 'True'
     ADMIN_ID = os.environ.get('OFFICIAL_ADMIN_ID')
     CKEDITOR_PKG_TYPE = 'standard-all'
     SR_PER_PAGE = 3 # Search results per page
-    
+
