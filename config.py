dfabae68f4816210802984df70ed20467c067f4e
dankeschoen13 <marcobernacer@gmail.com>
2026-05-01 15:03:53 +0800

    chore!: install pre-deployment dependency and create requirements.txt

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 
+db_url = os.environ.get('DATABASE_URL')
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-only-not-safe')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///cafes.db')
-    CKEDITOR_PKG_TYPE = 'standard-all'
-    SR_PER_PAGE = 3 # Search results per page
+    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///cafes.db"
     DEMO_MODE = os.environ.get('DEMO_MODE') == 'True'
     ADMIN_ID = os.environ.get('OFFICIAL_ADMIN_ID')
+    CKEDITOR_PKG_TYPE = 'standard-all'
+    SR_PER_PAGE = 3 # Search results per page
+    
